3c22f1941ac06a788fdbd8bb577835c35e634f53
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-26 22:19:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   get "mulitply/:num1/:num2" do
     number_1 = params[:num1].to_i
     number_2 = params[:num2].to_i
-    total = "#{number_1} * #{number_2}"
-    total
+    "#{number_1} * #{number_2}""
+    
   end
 end
