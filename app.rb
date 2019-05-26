375e40eb0aa2f2a16db96e00ac9ead2f647bf021
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-26 22:20:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     "Goodbye, #{name}."
   end
 
-  get "mulitply/:num1/:num2" do
+  get "multiply/:num1/:num2" do
     @num1 = params[:num1].to_i
     @num2 = params[:num2].to_i
     total = "#{@num1} * #{@num2}"
